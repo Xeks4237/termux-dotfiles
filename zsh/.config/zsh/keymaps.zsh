71e7b081fdcbcfccee197e2bcd3baac7807ea9d1
Xeks4237 <amid.muradov111@gmail.com>
2025-11-27 13:07:05 +0400

    Improved zsh keymaps

--- zsh/.config/zsh/keymaps.zsh
+++ zsh/.config/zsh/keymaps.zsh
@@ -10,10 +10,17 @@ bindkey "^[[H" beginning-of-line
 bindkey "^[[F" end-of-line
 
 # Map down arrow key to open fzf history search
-bindkey "^[[B" fzf-history-widget
+# bindkey "^[[B" fzf-history-widget
 
 # Fish like history search/suggestions
-bindkey '^[[A' history-substring-search-up
+bindkey "^[[A" history-substring-search-up
+bindkey "^[[B" history-substring-search-down
+
+# Sets PageUp key to
+# bindkey "^[[5~"
+
+# Sets PageDown key to open fzf history search widget
+bindkey "^[[6~" fzf-history-widget
 
 # etc
 bindkey "^[w" kill-region

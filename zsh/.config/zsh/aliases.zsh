7b2c6e5a0962382c22bb6254a18d116055f3fd93
Xeks4237 <amid.muradov111@gmail.com>
2025-12-02 20:06:19 +0400

    Minor Changes

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,15 +1,12 @@
 # [ Aliases ]
 
+# Alias to make ls look better
 alias ls="ls --almost-all --color=always --classify=always --group-directories-first --human-readable"
 
-# tmp keymap to force myself to use tree instead of ls
-alias ls="echo 'nuh uh >:3, use tree instead!'"
-
-alias cat="cat -n"
-
+# Alias to make wine-stable package command more convenient
 alias wine="wine-stable"
 
-# NOTE: I know that I added two -h in alias
+# NOTE:
 # Add -gu flag to tree in case to see file owners of files
 # And -D for date, -h for file size, and etc
 alias tree="tree --opt-toggle -aphCF -L1 --metafirst --dirsfirst --du"

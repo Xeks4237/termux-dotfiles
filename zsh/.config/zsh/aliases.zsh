416474ec4635a9e9ccb000ca1bb25e675beb226a
Xeks4237 <amid.muradov111@gmail.com>
2025-12-03 12:06:54 +0400

    Minor Changes, Switched to eza instead of tree and ls

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,13 +1,17 @@
 # [ Aliases ]
 
-# Alias to make ls look better
+# NOTE: I use eza instead of ls and tree, but I donmt erated aliases for them
+
+# Alias for ls
 alias ls="ls --almost-all --color=always --classify=always --group-directories-first --human-readable"
+
+# Alias for tree
+alias tree="tree --opt-toggle -aphCF -L1 --metafirst --dirsfirst --du"
+
+# Alias for eza
+alias eza="eza --across --almost-all --classify=always --color=always --context --extended --git --git-repos --grid --group-directories-first --header --icons=always --level=1 --long --no-filesize --no-time --sort=Name --time-style=long-iso --total-size --tree"
 
 # Alias to make wine-stable package command more convenient
 alias wine="wine-stable"
 
-# NOTE:
-# Add -gu flag to tree in case to see file owners of files
-# And -D for date, -h for file size, and etc
-alias tree="tree --opt-toggle -aphCF -L1 --metafirst --dirsfirst --du"
 

c090ba51759f01123f8dc75344212f1dc6bc4c39
Xeks4237 <amid.muradov111@gmail.com>
2025-12-20 10:58:18 +0400

    Changes "^[" to more easier to read "\e"

--- zsh/.config/zsh/keymaps.zsh
+++ zsh/.config/zsh/keymaps.zsh
@@ -3,24 +3,21 @@
 bindkey -v
 
 # Sets Home key to move cursor to the beginning of line
-bindkey "^[[H" beginning-of-line
+bindkey "\e[H" beginning-of-line
 
 # Sets End key to move cursor to the beginning of line
-bindkey "^[[F" end-of-line
+bindkey "\e[F" end-of-line
 
 # Map down arrow key to open fzf history search
-# bindkey "^[[B" fzf-history-widget
+# bindkey "\e[B" fzf-history-widget
 
 # Fish like history search/suggestions
-bindkey "^[[A" history-substring-search-up
-bindkey "^[[B" history-substring-search-down
-
-# Sets PageUp key to
-# bindkey "^[[5~"
+bindkey "\e[A" history-substring-search-up
+bindkey "\e[B" history-substring-search-down
 
 # Sets PageDown key to open fzf history search widget
-bindkey "^[[6~" fzf-history-widget
+bindkey "\e[6~" fzf-history-widget
 
 # etc
-bindkey "^[w" kill-region
-bindkey "^[[3~" delete-char
+bindkey "\ew" kill-region
+bindkey "\e[3~" delete-char

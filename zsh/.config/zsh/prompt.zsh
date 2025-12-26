759ad08325de5dc54b2d3e44cc8a9430842ce79a
Xeks4237 <amid.muradov111@gmail.com>
2025-12-26 10:10:25 +0400

    Minor Changes

--- zsh/.config/zsh/prompt.zsh
+++ zsh/.config/zsh/prompt.zsh
@@ -4,7 +4,7 @@
 # Licensed under: MIT License
 
 # Zsh substitutions:
-# %F{} => Colors everything after it, can use HEX, and mono RGB like #000000 and 000 values
+# %F{} => Colors everything after it
 # %f => Resets coloring after itself
 # %~ => Current path relative to $HOME directory
 # %n => Shows $USERNAME of the surrent user
@@ -22,48 +22,50 @@
 # \e[2K => clear everything on the current line
 
 # [ Configuration Variables ]
-# NOTE: I used Catppuccin Mocha Colors from https://github.com/catppuccin
+PROMPT_ZSHGOD_TMUX=${PROMPT_ZSHGOD_TMUX-"t "}
+PROMPT_ZSHGOD_PATH_PERCENT=${PROMPT_ZSHGOD_PATH_PERCENT-60}
+PROMPT_ZSHGOD_NOTITLE=${PROMPT_ZSHGOD_NOTITLE-0}
+PROMPT_ZSHGOD_CMD_MAX_EXEC_TIME=5
+PROMPT_ZSHGOD_ABBR_METHOD=${PROMPT_ZSHGOD_ABBR_METHOD-"truncate"}
+PROMPT_ZSHGOD_VCS=${PROMPT_ZSHGOD_VCS-1}
+PROMPT_ZSHGOD_PWD=${PROMPT_ZSHGOD_PWD-1}
+
+# NOTE: I used Catppuccin Mocha Colors from:
+# https://github.com/catppuccin
 # Main colors
-thm_rosewater="#f5e0dc"
-thm_flamingo="#f2cdcd"
-thm_pink="#f5c2e7"
-thm_mauve="#cba6f7"
-thm_red="#f38ba8"
-thm_maroon="#eba0ac"
-thm_peach="#fab387"
-thm_yellow="#f9e2af"
-thm_green="#a6e3a1"
-thm_teal="#94e2d5"
-thm_sky="#89dceb"
-thm_sapphire="#74c7ec"
-thm_blue="#89b4fa"
-thm_lavender="#b4befe"
+PROMPT_ZSHGOD_THM_rosewater="#f5e0dc"
+PROMPT_ZSHGOD_THM_flamingo="#f2cdcd"
+PROMPT_ZSHGOD_THM_pink="#f5c2e7"
+PROMPT_ZSHGOD_THM_mauve="#cba6f7"
+PROMPT_ZSHGOD_THM_red="#f38ba8"
+PROMPT_ZSHGOD_THM_maroon="#eba0ac"
+PROMPT_ZSHGOD_THM_peach="#fab387"
+PROMPT_ZSHGOD_THM_yellow="#f9e2af"
+PROMPT_ZSHGOD_THM_green="#a6e3a1"
+PROMPT_ZSHGOD_THM_teal="#94e2d5"
+PROMPT_ZSHGOD_THM_sky="#89dceb"
+PROMPT_ZSHGOD_THM_sapphire="#74c7ec"
+PROMPT_ZSHGOD_THM_blue="#89b4fa"
+PROMPT_ZSHGOD_THM_lavender="#b4befe"
 
 # Main surface and overlay colors
-thm_subtext_1="#a6adc8"
-thm_subtext_0="#bac2de"
-thm_fg="#cdd6f4"
-thm_overlay_2="#9399b2"
-thm_overlay_1="#7f849c"
-thm_overlay_0="#6c7086"
-thm_surface_2="#585b70"
-thm_surface_1="#45475a"
-thm_surface_0="#313244"
-thm_mantle="#181825"
-thm_crust="#11111b"
-thm_bg="#1e1e2e"
+PROMPT_ZSHGOD_THM_subtext_1="#a6adc8"
+PROMPT_ZSHGOD_THM_subtext_0="#bac2de"
+PROMPT_ZSHGOD_THM_fg="#cdd6f4"
+PROMPT_ZSHGOD_THM_overlay_2="#9399b2"
+PROMPT_ZSHGOD_THM_overlay_1="#7f849c"
+PROMPT_ZSHGOD_THM_overlay_0="#6c7086"
+PROMPT_ZSHGOD_THM_surface_2="#585b70"
+PROMPT_ZSHGOD_THM_surface_1="#45475a"
+PROMPT_ZSHGOD_THM_surface_0="#313244"
+PROMPT_ZSHGOD_THM_mantle="#181825"
+PROMPT_ZSHGOD_THM_crust="#11111b"
+PROMPT_ZSHGOD_THM_bg="#1e1e2e"
 
-PROMPT_LEAN_TMUX=${PROMPT_LEAN_TMUX-"t "}
-PROMPT_LEAN_PATH_PERCENT=${PROMPT_LEAN_PATH_PERCENT-60}
-PROMPT_LEAN_NOTITLE=${PROMPT_LEAN_NOTITLE-0}
-PROMPT_LEAN_CMD_MAX_EXEC_TIME=5
-PROMPT_LEAN_ABBR_METHOD=${PROMPT_LEAN_ABBR_METHOD-"truncate"}
-PROMPT_LEAN_VCS=${PROMPT_LEAN_VCS-1}
-PROMPT_LEAN_PWD=${PROMPT_LEAN_PWD-1}
-
+# [ Functions ]
 prompt_zshgod_help() {
     cat <<"EOF"
-Lean is a one line prompt that tries to stay out of your face. It utilizes
+ZshGod is a one line prompt that tries to stay out of your face. It utilizes
 the right side prompt for most information, like the current working directory
 and version control system (only Git)info. The left side of the prompt is only
 a "%". The only other information shown on the left are the jobs numbers of
@@ -77,27 +79,27 @@ You can invoke it thus:
 
 Several aspects Lean can be configured:
 
-PROMPT_LEAN_TMUX:   used to indicate being in tmux, set to "t " by default
-PROMPT_LEAN_LEFT:   executed to allow custom information in the left side
-PROMPT_LEAN_RIGHT:  executed to allow custom information in the right side
-PROMPT_LEAN_VIMODE: used to determine whether or not to display indicator
-PROMPT_LEAN_PWD:    when set to 0, disables showing CWD in the prompt
-PROMPT_LEAN_VCS:    when set to 0, disables git details from the prompt, the
+PROMPT_ZSHGOD_TMUX:   used to indicate being in tmux, set to "t " by default
+PROMPT_ZSHGOD_LEFT:   executed to allow custom information in the left side
+PROMPT_ZSHGOD_RIGHT:  executed to allow custom information in the right side
+PROMPT_ZSHGOD_VIMODE: used to determine whether or not to display indicator
+PROMPT_ZSHGOD_PWD:    when set to 0, disables showing CWD in the prompt
+PROMPT_ZSHGOD_VCS:    when set to 0, disables git details from the prompt, the
                     branch name is truncated if it's longer than 20
                     characters
-PROMPT_LEAN_VIMODE_FORMAT:
+PROMPT_ZSHGOD_VIMODE_FORMAT:
                     defaults to "%F{red}[NORMAL]%f"
-PROMPT_LEAN_NOTITLE:
+PROMPT_ZSHGOD_NOTITLE:
                     used to determine wether or not to set title, set to 0
                     by default
-PROMPT_LEAN_ABBR_METHOD:
+PROMPT_ZSHGOD_ABBR_METHOD:
                     used to indicate the abbreviation method for directory
                     paths. Set it either to "truncate" (default) or "shrink"
                     (fish-style working directory)
 EOF
 }
 
-# Turns seconds into human readable time, 165392 => 1d 21h 56m 32s
+# turns seconds into human readable time, 165392 => 1d 21h 56m 32s
 prompt_zshgod_human_time() {
     local tmp=$1
     local days=$(( tmp / 60 / 60 / 24 ))
@@ -110,23 +112,23 @@ prompt_zshgod_human_time() {
     echo "${seconds}s "
 }
 
-# Displays the exec time of the last command if set threshold was exceeded
+# displays the exec time of the last command if set threshold was exceeded
 prompt_zshgod_cmd_exec_time() {
     local stop=$EPOCHSECONDS
     local start=${cmd_timestamp:-$stop}
     integer elapsed=$stop-$start
-    (($elapsed > ${PROMPT_LEAN_CMD_MAX_EXEC_TIME})) && prompt_zshgod_human_time $elapsed
+    (($elapsed > ${PROMPT_ZSHGOD_CMD_MAX_EXEC_TIME})) && prompt_zshgod_human_time $elapsed
 }
 
-# Fastest possible way to check if repo is dirty
+# fastest possible way to check if repo is dirty
 prompt_zshgod_git_dirty() {
-    if [[ $PROMPT_LEAN_VCS != 1 ]]; then
+    if [[ $PROMPT_ZSHGOD_VCS != 1 ]]; then
         return
     fi
     # check if we're in a git repo
     command git rev-parse --is-inside-work-tree &>/dev/null || return
     # check if it's dirty
-    local umode="-uno" # || local umode="-unormal"
+    local umode="-uno" #|| local umode="-unormal"
     command test -n "$(git status --porcelain --ignore-submodules ${umode} 2>/dev/null | head -100)"
 
     (($? == 0)) && echo "*"
@@ -143,13 +145,13 @@ prompt_zshgod_set_title() {
 
 prompt_zshgod_preexec() {
     typeset -g cmd_timestamp=$EPOCHSECONDS
-    (($PROMPT_LEAN_NOTITLE != 1)) && prompt_zshgod_set_title "$1"
+    (($PROMPT_ZSHGOD_NOTITLE != 1)) && prompt_zshgod_set_title "$1"
 }
 
 prompt_zshgod_pwd() {
     local lean_path='$(print -Pn "%~")'
-    if (($#lean_path / $COLUMNS.0 * 100 > ${PROMPT_LEAN_PATH_PERCENT:=60})); then
-        case "$PROMPT_LEAN_ABBR_METHOD" in
+    if (($#lean_path / $COLUMNS.0 * 100 > ${PROMPT_ZSHGOD_PATH_PERCENT:=60})); then
+        case "$PROMPT_ZSHGOD_ABBR_METHOD" in
             "truncate") prompt_zshgod_abbr_truncate ;;
             "shrink")   prompt_zshgod_abbr_shrink ;;
         esac
@@ -166,7 +168,7 @@ prompt_zshgod_abbr_shrink() {
     setopt local_options extendedglob histsubstpattern
 
     local lean_path=$(print -Pn "%~")
-    local maxlen=$((PROMPT_LEAN_PATH_PERCENT * COLUMNS / 100))
+    local maxlen=$((PROMPT_ZSHGOD_PATH_PERCENT * COLUMNS / 100))
     local prevlen=0
 
     # iterate until target length achieved or no more abbreviation possible
@@ -179,7 +181,7 @@ prompt_zshgod_abbr_shrink() {
 }
 
 prompt_zshgod_precmd() {
-    [[ $PROMPT_LEAN_VCS == 1 ]] && vcs_info 2>/dev/null
+    [[ $PROMPT_ZSHGOD_VCS == 1 ]] && vcs_info 2>/dev/null
     rehash
 
     local jobs
@@ -189,28 +191,28 @@ prompt_zshgod_precmd() {
         j=$jobstates[$a];i="${${(@s,:,)j}[2]}"
         jobs+=($a${i//[^+-]/})
     }
-    # Print with [ ] and comma separated
+    # print with [ ] and comma separated
     prompt_zshgod_jobs=""
-    [[ -n $jobs ]] && prompt_zshgod_jobs="%F{"$thm_green"}["${(j:,:)jobs}"] "
+    [[ -n $jobs ]] && prompt_zshgod_jobs="%F{"$PROMPT_ZSHGOD_THM_green"}["${(j:,:)jobs}"] "
 
     local lean_vimode_default="%F{red}[NORMAL]%f"
-    # If LEAN_VIMODE is set, set lean_vimode_indicator to either PROMPT_LEAN_VIMOD_FORMAT or a default value
-    local lean_vimode_indicator="${PROMPT_LEAN_VIMODE:+${PROMPT_LEAN_VIMODE_FORMAT:-${lean_vimode_default}}}"
+    #If LEAN_VIMODE is set, set lean_vimode_indicator to either PROMPT_ZSHGOD_VIMOD_FORMAT or a default value
+    local lean_vimode_indicator="${PROMPT_ZSHGOD_VIMODE:+${PROMPT_ZSHGOD_VIMODE_FORMAT:-${lean_vimode_default}}}"
 
     prompt_zshgod_vimode="${${KEYMAP/vicmd/$lean_vimode_indicator}/(main|viins)/}"
 
     setopt promptsubst
     local vcs_info_str=""
-    [[ $PROMPT_LEAN_VCS == 1 ]] && vcs_info_str="$vcs_info_msg_0_" # avoid https://github.com/njhartwell/pw3nage
-    PROMPT="$prompt_zshgod_jobs%F{$thm_green}${prompt_zshgod_tmux}%f$($PROMPT_LEAN_LEFT)%f%(?.%F{$thm_blue}.%B%F{203})%#%f%k%b "
+    [[ $PROMPT_ZSHGOD_VCS == 1 ]] && vcs_info_str="$vcs_info_msg_0_" # avoid https://github.com/njhartwell/pw3nage
+    PROMPT="$prompt_zshgod_jobs%F{$PROMPT_ZSHGOD_THM_green}${prompt_zshgod_tmux}%f$($PROMPT_ZSHGOD_LEFT)%f%(?.%F{$PROMPT_ZSHGOD_THM_blue}.%B%F{203})%#%f%k%b "
 
     local lean_pwd=""
-    [[ $PROMPT_LEAN_PWD == 1 ]] && lean_pwd=$(prompt_zshgod_pwd)
-    RPROMPT="%F{$thm_yellow}$(prompt_zshgod_cmd_exec_time)%f$prompt_zshgod_vimode%F{$thm_blue}$lean_pwd%F{$thm_green}$vcs_info_str$(prompt_zshgod_git_dirty)$prompt_zshgod_host%f$($PROMPT_LEAN_RIGHT)%f"
+    [[ $PROMPT_ZSHGOD_PWD == 1 ]] && lean_pwd=$(prompt_zshgod_pwd)
+    RPROMPT="%F{$PROMPT_ZSHGOD_THM_yellow}$(prompt_zshgod_cmd_exec_time)%f$prompt_zshgod_vimode%F{$PROMPT_ZSHGOD_THM_blue}$lean_pwd%F{$PROMPT_ZSHGOD_THM_green}$vcs_info_str$(prompt_zshgod_git_dirty)$prompt_zshgod_host%f$($PROMPT_ZSHGOD_RIGHT)%f"
 
-    (($PROMPT_LEAN_NOTITLE != 1)) && prompt_zshgod_set_title "$1"
+    (($PROMPT_ZSHGOD_NOTITLE != 1)) && prompt_zshgod_set_title "$1"
 
-    unset cmd_timestamp # Reset value since "preexec" isn't always triggered
+    unset cmd_timestamp # reset value since "preexec" isn't always triggered
 }
 
 function zle-keymap-select {
@@ -223,21 +225,21 @@ prompt_zshgod_setup() {
 
     zmodload zsh/datetime
     autoload -Uz add-zsh-hook
-    [[ $PROMPT_LEAN_VCS == 1 ]] && autoload -Uz vcs_info
+    [[ $PROMPT_ZSHGOD_VCS == 1 ]] && autoload -Uz vcs_info
 
-    [[ "$PROMPT_LEAN_VIMODE" != "" ]] && zle -N zle-keymap-select
+    [[ "$PROMPT_ZSHGOD_VIMODE" != "" ]] && zle -N zle-keymap-select
 
     add-zsh-hook precmd prompt_zshgod_precmd
     add-zsh-hook preexec prompt_zshgod_preexec
 
-    if [[ $PROMPT_LEAN_VCS == 1 ]]; then
+    if [[ $PROMPT_ZSHGOD_VCS == 1 ]]; then
         zstyle ":vcs_info:*" enable git
         zstyle ":vcs_info:git*" formats " %25>...>%b%>>"
         zstyle ":vcs_info:git*" actionformats " %25>...>%b%>>|%a"
     fi
 
-    [[ "$SSH_CONNECTION" != "" ]] && prompt_zshgod_host=" %F{"$thm_yellow"}%m%f"
-    [[ "$TMUX" != "" ]] && prompt_zshgod_tmux=$PROMPT_LEAN_TMUX
+    [[ "$SSH_CONNECTION" != "" ]] && prompt_zshgod_host=" %F{"$PROMPT_ZSHGOD_THM_yellow"}%m%f"
+    [[ "$TMUX" != "" ]] && prompt_zshgod_tmux=$PROMPT_ZSHGOD_TMUX
 
     return 0
 }

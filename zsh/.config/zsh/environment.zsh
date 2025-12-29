90df9958b7df39b94b8ba513b620ef25d1d119c7
Xeks4237 <amid.muradov111@gmail.com>
2025-12-29 16:08:46 +0400

    Fixed some variable after reading manual

--- zsh/.config/zsh/environment.zsh
+++ zsh/.config/zsh/environment.zsh
@@ -31,7 +31,7 @@ EDITOR="$PREFIX/bin/nvim"
 VISUAL="$PREFIX/bin/nvim"
 
 # Editor which is used when editing current command in editor through zsh-vi-mode plugin
-ZVM_VI_EDITOR={$EDITOR:-$VISUAL}
+ZVM_VI_EDITOR=${EDITOR:-$VISUAL}
 
 # Enables system clipboard support for zsh-vi-mode plugin
 ZVM_SYSTEM_CLIPBOARD_ENABLED=true

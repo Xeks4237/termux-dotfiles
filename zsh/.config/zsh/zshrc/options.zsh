b17549c237b88993505670a399f78a148c749978
Xeks4237 <amid.muradov111@gmail.com>
2026-01-17 17:34:32 +0400

    Minor Changes

--- zsh/.config/zsh/zshrc/options.zsh
+++ zsh/.config/zsh/zshrc/options.zsh
@@ -5,9 +5,7 @@
 # This file is used for (un)setting options of your interactive zsh instances
 # And this file exists only for maintaining purposes
 
-
 # [ Disabled options for Zsh ]
-
 # If a command is issued that can't be executed as a normal
 # command, and the command is the name of a directory, perform
 # the cd command to that directory.  This option is only

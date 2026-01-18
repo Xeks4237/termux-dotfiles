95f4311c09d7cdb12f6561f76ae183f38f8faba0
Xeks4237 <amid.muradov111@gmail.com>
2026-01-18 13:57:54 +0400

    Moved zsh keymaps to seperate file

---
+++ zsh/.config/zsh/zshrc/keymaps.zsh
@@ -0,0 +1,50 @@
+#!/usr/bin/env zsh
+# NOTE: Shebang line isn't needed here, But I anyway added it for fun
+
+# This file is sourced in '.zshrc'
+# This file is meant for setting keymaps/keybindings of your interactive zsh instances
+# And this file exists only for maintaining purposes
+
+# Enable vi style keymaps
+bindkey -v
+
+# Sets Home key to move cursor to the beginning of line
+bindkey "\e[H" beginning-of-line
+bindkey "^[[1~" beginning-of-line
+
+# Sets End key to move cursor to the beginning of line
+bindkey "\e[F" end-of-line
+bindkey "^[[4~" end-of-line
+
+# Fish like history search/suggestions
+# Arrow up to search up
+bindkey "\e[A" history-substring-search-up
+# Arrow down to search down
+bindkey "\e[B" history-substring-search-down
+
+# Sets PageDown key to open fzf history search widget
+bindkey "\e[6~" fzf-history-widget
+
+# Simple function with Keymap to toggle termux wakelock state
+# File where this state is stores globally to don't cause error while using multiple shells
+export TERMUX_WAKE_LOCK_STATE_FILE="$XDG_DATA_HOME/termux/termux_wake_lock_state_file"
+# Function to do logic related to wakelock
+termux_wake_lock_toggle() {
+    local termux_wake_lock_state=$(cat $TERMUX_WAKE_LOCK_STATE_FILE)
+    if [[ $termux_wake_lock_state == 0 ]]; then
+        termux-wake-lock
+        echo "1" > $TERMUX_WAKE_LOCK_STATE_FILE
+    else
+        termux-wake-unlock
+        echo "0" > $TERMUX_WAKE_LOCK_STATE_FILE
+    fi
+}
+# Make zle widget from function
+zle -N termux_wake_lock_toggle_widget termux_wake_lock_toggle
+# Bind this widget to 'Ctrl h'
+bindkey "^h" termux_wake_lock_toggle_widget
+
+# Makes 'frontspace?' to delete 1 character after the cursor
+# Its like 'backspace' but deletes character after the cursor rather then before
+bindkey "\e[3~" delete-char
+

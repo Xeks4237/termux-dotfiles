a4af492626fd9583bf5534121a71708af6293465
Xeks4237 <amid.muradov111@gmail.com>
2026-02-03 18:59:58 +0400

    Minor Changes to zsh config aliases

--- zsh/.config/zsh/zshrc/aliases.zsh
+++ zsh/.config/zsh/zshrc/aliases.zsh
@@ -6,17 +6,19 @@
 # And this file exists only for maintaining purposes
 
 # Alias for ls
-alias ls="ls --almost-all --color=always --classify=always --group-directories-first --human-readable"
+alias ls='ls --almost-all --color=always --classify=always --group-directories-first --human-readable'
 
 # Alias for tree
-alias tree="tree --opt-toggle -aphCF -L1 --metafirst --dirsfirst --du"
+alias tree='tree --opt-toggle -aphCF -L1 --metafirst --dirsfirst --du'
 
 # Alias for eza
-alias eza="eza --across --almost-all --classify=always --color=always --context --extended --git --git-repos --grid --group-directories-first --header --icons=always --level=1 --long --no-time --sort=Name --time-style=long-iso --total-size"
+alias eza='eza --across --almost-all --classify=always --color=always --context --extended --git --git-repos --grid --group-directories-first --header --icons=always --level=1 --long --no-time --sort=Name --time-style=long-iso --total-size'
 
 # Alias to wine-stable
-alias wine="wine-stable"
+alias wine='wine-stable'
 
 # Alias for wget
 alias wget="wget --hsts-file=${XDG_DATA_HOME:-$HOME/.local/share/}/wget/wget-hsts"
 
+# Alias for rm
+alias rm='rm -v'

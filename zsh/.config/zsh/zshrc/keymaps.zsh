d5a8cbed26a8eac498b86994325a4292f2652af6
Xeks4237 <amid.muradov111@gmail.com>
2026-02-03 19:00:09 +0400

    Minor Changes to zsh config keymaps

--- zsh/.config/zsh/zshrc/keymaps.zsh
+++ zsh/.config/zsh/zshrc/keymaps.zsh
@@ -5,7 +5,7 @@
 # This file is meant for setting keymaps/keybindings of your interactive zsh instances
 # And this file exists only for maintaining purposes
 
-# [ Configuration/Code setted up using zkbd module of zsh ]
+# [ Keymaps setted up using zkbd module of zsh ]
 typeset -g -A key
 
 key[F1]='^[OP'
@@ -37,28 +37,26 @@ key[Right]='^[[C'
 bindkey -v
 
 # Sets Home key to move cursor to the beginning of line
-bindkey '\e[H' beginning-of-line
-bindkey '^[[1~' beginning-of-line
+[[ -n ${key[Home]} ]] && bindkey ${key[Home]} beginning-of-line
+
 # Same thing but for vi normal modes
-bindkey -M vicmd '\e[H' beginning-of-line
-bindkey -M vicmd '^[[1~' beginning-of-line
+[[ -n ${key[Home]} ]] && bindkey -M vicmd ${key[Home]} beginning-of-line
 
 # Sets End key to move cursor to the beginning of line
-bindkey '\e[F' end-of-line
-bindkey '^[[4~' end-of-line
-# Same thing but for vi normal modes
-bindkey -M vicmd '\e[F' end-of-line
-bindkey -M vicmd '^[[4~' end-of-line
+[[ -n ${key[End]} ]] && bindkey ${key[End]} end-of-line
 
-# Sets PageDown key to open fzf history search widget
-bindkey '\e[6~' fzf-history-widget
+# Same thing but for vi normal mode
+[[ -n ${key[End]} ]] && bindkey -M vicmd ${key[End]} end-of-line
+
+# Sets Ctrl r to open fzf history search widget
+bindkey '^r' fzf-history-widget
 
 # Makes 'frontspace?' to delete 1 character after the cursor
 # Its like 'backspace' but deletes character after the cursor rather then before
-bindkey '\e[3~' delete-char
+[[ -n ${key[Delete]} ]] && bindkey ${key[Delete]} delete-char
 
 # Makes/Fixes backspace to work after returning to insert mode from normal mode
-bindkey -M viins '^?' backward-delete-char
+[[ -n ${key[Delete]} ]] && bindkey -M viins ${key[Delete]} backward-delete-char
 
 # Simple function with Keymap to toggle termux wakelock state
 # File where this state is stores globally to don't cause error while using multiple shells

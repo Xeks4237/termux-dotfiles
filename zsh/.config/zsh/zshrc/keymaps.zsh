7a891fbe61180bcc8d3977d0a1574ae6e02ffdd6
Xeks4237 <amid.muradov111@gmail.com>
2026-02-03 20:35:42 +0400

    Added zkbd configured file from zsh zkbd module and minor changes to keymaps.zsh

--- zsh/.config/zsh/zshrc/keymaps.zsh
+++ zsh/.config/zsh/zshrc/keymaps.zsh
@@ -6,31 +6,7 @@
 # And this file exists only for maintaining purposes
 
 # [ Keymaps setted up using zkbd module of zsh ]
-typeset -g -A key
-
-key[F1]='^[OP'
-key[F2]='^[OQ'
-key[F3]='^[OR'
-key[F4]='^[OS'
-key[F5]='^[[15~'
-key[F6]='^[[17~'
-key[F7]='^[[18~'
-key[F8]='^[[19~'
-key[F9]='^[[20~'
-key[F10]='^[[21~'
-key[F11]='^[[23~'
-key[F12]='^[[24~'
-key[Backspace]='^?'
-key[Insert]='^[[2~'
-key[Home]='^[[1~'
-key[PageUp]='^[[5~'
-key[Delete]='^?'
-key[End]='^[[4~'
-key[PageDown]='^[[6~'
-key[Up]='^[[A'
-key[Left]='^[[D'
-key[Down]='^[[B'
-key[Right]='^[[C'
+source "${ZDOTDIR:-$XDG_CONFIG_HOME/zsh/}/.zkbd/$TERM-${${DISPLAY:t}:-$VENDOR-$OSTYPE}"
 
 # [ Custom keymaps ]
 # Enable vi style keymaps
@@ -53,10 +29,10 @@ bindkey '^r' fzf-history-widget
 
 # Makes 'frontspace?' to delete 1 character after the cursor
 # Its like 'backspace' but deletes character after the cursor rather then before
-[[ -n ${key[Delete]} ]] && bindkey ${key[Delete]} delete-char
+[[ -n ${key[Delete]} ]] && bindkey "${key[Delete]}" delete-char
 
 # Makes/Fixes backspace to work after returning to insert mode from normal mode
-[[ -n ${key[Delete]} ]] && bindkey -M viins ${key[Delete]} backward-delete-char
+[[ -n ${key[Delete]} ]] && bindkey -M viins "${key[Delete]}" backward-delete-char
 
 # Simple function with Keymap to toggle termux wakelock state
 # File where this state is stores globally to don't cause error while using multiple shells

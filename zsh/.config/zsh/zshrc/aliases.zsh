c3dc4fe060fbb4b9fc75d0f1e04c2944987d943b
Xeks4237 <amid.muradov111@gmail.com>
2026-02-15 18:38:01 +0400

    Minor Changes to zsh config's aliases.zsh

--- zsh/.config/zsh/zshrc/aliases.zsh
+++ zsh/.config/zsh/zshrc/aliases.zsh
@@ -12,7 +12,7 @@ alias ls='ls --almost-all --color=always --classify=always --group-directories-f
 alias tree='tree --opt-toggle -aphCF -L1 --metafirst --dirsfirst --du'
 
 # Alias for eza
-alias eza='eza --across --almost-all --classify=always --color=always --context --extended --git --git-repos --grid --group-directories-first --header --icons=always --level=1 --long --no-time --sort=Name --time-style=long-iso --total-size'
+alias eza='eza --across --almost-all --classify=always --color=always --context --extended --git --git-repos --grid --group-directories-first --header --icons=always --level=1 --long --no-time --sort=Name --time-style=long-iso --total-size --no-permissions --octal-permissions'
 
 # Alias to wine-stable
 alias wine='wine-stable'
@@ -22,3 +22,6 @@ alias wget="wget --hsts-file=${XDG_DATA_HOME:-$HOME/.local/share/}/wget/wget-hst
 
 # Alias for rm
 alias rm='rm -v'
+
+# Alias for builtin stat alternative of zsh
+alias zstat="zstat -tsrnL -F'%T %Z %d/%m/%Y'"

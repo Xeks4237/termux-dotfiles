c274ebe7fce0d6bfac4d5d7e04c37a5f82faee44
Xeks4237 <amid.muradov111@gmail.com>
2026-02-16 18:37:58 +0400

    Minor Changes

--- scripts/Scripts/update.zsh
+++ scripts/Scripts/update.zsh
@@ -3,16 +3,9 @@
 print -P '%B%F{#A6E3A1}Starting Update...%f%b'
 pkg update -y
 pkg upgrade -y
-print -P '%B%F{#A6E3A1}Update is finished.%f%b'
-
-print -P '%B%F{#A6E3A1}Cleaning mess of pkg/apt...%f%b'
 pkg autoclean
 pkg clean
-print -P '%B%F{#A6E3A1}Finished cleaning mess of pkg/apt.%f%b'
-
-print -P '%B%F{#A6E3A1}Starting update of nerd fonts through getnf...%f%b'
 getnf -U
 rm -rf ~/share/
-print -P '%B%F{#A6E3A1}Fonts are updated.%f%b'
 
 print -P '%F{#B4BEFE}zsh:%f%F{#A6E3A1} update.sh script is succesfully finished.%f'

34c378bf642d398740f502ac005c9ccca1ce8202
Xeks4237 <amid.muradov111@gmail.com>
2026-03-21 16:46:07 +0400

    Improved ultimate-push script a little bit by changing it from sh to zsh

--- scripts/Scripts/ultimate-push.zsh
+++ scripts/Scripts/ultimate-push.zsh
@@ -1,12 +1,15 @@
-#!/usr/bin/sh
-# Simple shell script to push changes to remote repo
+#!/usr/bin/zsh
+# Simple zsh script to push changes to remote repo
 # Because I most oftenly push stuff to my repos at same period of time
 
+print -P '%F{green}Pushing dotfiles...%f'
 cd ~/.dotfiles/
 git push codeberg main
 
+print -P '%F{green}Pushing ZshGod...%f'
 cd ~/Git/zshgod/
 git push codeberg main
 
+print -P '%F{green}Pushing NV-GOD...%f'
 cd ~/.dotfiles/nvim/.config/nvim/
 git push codeberg main

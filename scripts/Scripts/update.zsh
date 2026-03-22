befbd2f829b0ff4f5c2ef0b59e15f3e77b10af95
Xeks4237 <amid.muradov111@gmail.com>
2026-03-22 14:44:06 +0400

    Improved a little update.zsh script

--- scripts/Scripts/update.zsh
+++ scripts/Scripts/update.zsh
@@ -1,12 +1,14 @@
 #!/usr/bin/zsh
+# Simple script to update system and stuff in one command
 
-print -P '%B%F{#A6E3A1}Starting Update...%f%b'
-
+print -P '%F{green}Updating Termux%f'
 pkg update -y
 pkg upgrade -y
+
+print -P '%F{green}Updating nerd fonts%f'
+getnf -U
+
+print -P '%F{cyan}Cleaning%f'
+rm -rf ~/share/
 pkg autoclean
 pkg clean
-getnf -U
-rm -rf ~/share/
-
-print -P '%F{#A6E3A1}update.zsh script is succesfully finished%f'

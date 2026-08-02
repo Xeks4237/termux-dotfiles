9d6b6f25fa8ba5283fb60601d62f1cb3f6f70114
Xeks4237 <amid.muradov111@gmail.com>
2026-08-02 23:10:51 +0400

    Minor Changes to ultimate-push.zsh

--- scripts/.local/bin/ultimate-push.zsh
+++ scripts/.local/bin/ultimate-push.zsh
@@ -16,13 +16,6 @@ print -P '%F{green}Pushing ZshGod...%f'
 git push codeberg main
 
 print ''
-print -P '%F{green}Zflex:%f'
-cd ~/Git/Zflex/
-git status --short
-print -P '%F{green}Pushing Zflex...%f'
-git push codeberg main
-
-print ''
 print -P '%F{green}NV-GOD:%f'
 cd ~/.dotfiles/nvim/.config/nvim/
 git status --short

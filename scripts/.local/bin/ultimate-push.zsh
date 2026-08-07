8004846c39a80b2e372d3fabf2866b3e519f517e
Xeks4237 <amid.muradov111@gmail.com>
2026-08-07 15:02:54 +0400

    Updated ultimate-push.zsh because I anyway have one remote now

--- scripts/.local/bin/ultimate-push.zsh
+++ scripts/.local/bin/ultimate-push.zsh
@@ -6,25 +6,25 @@ print -P '%F{green}dotfiles:%f'
 cd ~/.dotfiles/
 git status --short
 print -P '%F{green}Pushing dotfiles...%f'
-git push codeberg main
+git push origin main
 
 print ''
 print -P '%F{green}ZshGod:%f'
 cd ~/Git/ZshGod/
 git status --short
 print -P '%F{green}Pushing ZshGod...%f'
-git push codeberg main
+git push origin main
 
 print ''
 print -P '%F{green}Colornstant:%f'
 cd ~/Git/colornstant/
 git status --short
 print -P '%F{green}Pushing Colornstant...%f'
-git push codeberg main
+git push origin main
 
 print ''
 print -P '%F{green}NV-GOD:%f'
 cd ~/.dotfiles/nvim/.config/nvim/
 git status --short
 print -P '%F{green}Pushing NV-GOD...%f'
-git push codeberg main
+git push origin main
